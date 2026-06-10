69016154b005f49bc5c151c1d8e1525cf9027e6d
sym776 <symsym776@163.com>
2026-06-10 17:49:53 +0800

    load weapons
    unfinished

--- shop_card.gd
+++ shop_card.gd
@@ -1,8 +1,19 @@
 extends Panel
 class_name ShopCard
 
+@export var shop_item: ItemBase:set = _set_shop_item
+
+
 @onready var item_icon: TextureRect = $MarginContainer/Control/ItemIcon
 @onready var item_name: Label = $MarginContainer/Control/ItemName
 @onready var item_type: Label = $MarginContainer/Control/ItemType
 @onready var item_description: RichTextLabel = $MarginContainer/Control/ItemDescription
 @onready var coins_label: Label = %CoinsLabel
+
+func _set_shop_item(value:ItemBase) -> void:
+	shop_item = value
+	item_icon.texture = value.item_icon
+	item_name.text = value.item_name
+	item_type.text = ItemBase.ItemType.keys()[value.item_type]
+	#未完
+	
